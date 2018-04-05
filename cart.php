710531575f8dba584ffe9c64e4443c569d896d33
aberinger <aymeric@beringer.cf>
2018-04-05 02:48:37 +0000

    Added remove and update functionality

--- cart.php
+++ cart.php
@@ -16,11 +16,13 @@ function getAuthor($book) {
 
 function displayCart() {
 	echo "<table>";
-	echo "<tr><th>Cover</th><th>Title</th><th>Description</th><th>Author</th></tr>";
+	echo "<tr><th>Cover</th><th>Title</th><th>Description</th><th>Author</th><th>Quantity</th></tr>";
 	foreach ($_SESSION["cart"] as $book) {
+		$id = $book["bookID"];
 		$img = $book["bookImage"];
 		$title = $book["bookName"];
 		$desc = $book["bookDescription"];
+		$qty = $book["quantity"];
 		$author = getAuthor($book);
 		$authorName = $author["authorName"];
 
@@ -29,10 +31,25 @@ function displayCart() {
 		echo "<td><h3>$title</h3></td>";
 		echo "<td>$desc</td>";
 		echo "<td>$authorName</td>";
+		echo "<td><form method='post'><input type='hidden' value='$id' name='changeQtyId'>" .
+			"<input type='text' name='quantity' value='$qty'><submit value='Update'></form></td>";
+		echo "<td><form method='post'><input type='hidden' value='$id' name='removeId'>" .
+			"<submit value='Remove'></form></td>";
 		echo "</tr>";
 	}
 	echo "</table>";
 }
+
+if (isset($_POST["removeId"]))
+	foreach ($_SESSION["cart"] as $key => $book)
+		if ($book["bookID"] == $_POST["removeId"])
+			unset($_SESSION["cart"][$key]);
+
+if (isset($_POST["changeQtyId"]))
+	foreach ($_SESSION["cart"] as $key => $book)
+		if ($book["bookID"] == $_POST["changeQtyId"])
+			$_SESSION["cart"][$key]["quantity"] = $_POST["quantity"];
+
 ?>
 
 <!DOCTYPE html>
