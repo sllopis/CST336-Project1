a4e8f84df565c81fae4f396aa01e04b13f41836c
Sean Quevedo <squevedo@csumb.edu>
2018-04-11 01:24:48 +0000

    Updating index.php

--- index.php
+++ index.php
@@ -4,6 +4,44 @@
     include 'dbConnection.php';
     
     $conn = getDatabaseConnection("tp");
+    
+    function displayAuthor(){
+        global $conn;
+        
+        $sql = "SELECT authorID, authorName FROM `author` ORDER BY authorName";
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        //print_r($records);
+        
+        foreach ($records as $record) {
+            
+            echo "<option value='".$record["authorID"]."' >" . $record["authorName"] . "</option>";
+            
+        }
+        
+    }
+    
+    function displayCategories(){
+        global $conn;
+        
+        $sql = "SELECT catId, catName FROM `category` ORDER BY catName";
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        //print_r($records);
+        
+        foreach ($records as $record) {
+            
+            echo "<option value='".$record["catId"]."' >" . $record["catName"] . "</option>";
+            
+        }
+        
+    }
 
     function displaySearchResults(){
         global $conn;
@@ -22,17 +60,20 @@
             
             $sql = "SELECT bookId, bookName, bookDescription FROM book "; //dont need WHERE 1?
             
-            
-            if(isset($_GET['orderBy'])) {
+            if(isset($_GET[])) {
                 
+            }
+            if(isset($_GET['sortBy'])) {
+                
+                $sql = "SELECT authorName, bookId, bookName, bookDescription FROM book ";
                 //SELECT * FROM book INNER JOIN category ON book.categoryID = category.catID ORDER BY category.catName
                 //join the authorID to author table
                 if($_GET['orderBy'] == "author") {
-                    $sql .= " INNER JOIN author ON book.authorID = author.authorID ORDER BY author.authorName";
+                    $sql .= " INNER JOIN author ON book.authorID = author.authorID ORDER BY author.authorName ";
                 }
                 
                 if($_GET['orderBy'] == "book") {
-                    $sql .= " WHERE 1 ORDER BY bookName";
+                    $sql .= " WHERE 1 ORDER BY bookName ";
                 }
                 
                 //join the categoryID to category table
@@ -41,18 +82,28 @@
                 // FROM book 
                 // NATURAL JOIN category
                 // ON book.categoryID = catID
-                if($_GET['orderBy'] == "category") {
-                    $sql .= " ORDER BY category";
-                }
+                // if($_GET['orderBy'] == "category") {
+                //     $sql .= " ORDER BY category ";
+                // }
                 
                 
             }
+            
+            if($_GET["order"] == "asc") {
+                     $sql = "SELECT bookId, bookName, bookDescription FROM book ";
+                    $sql .= " ";
+                }
+                
+                if($_GET["order"] == "desc") {
+                    $sql .= " DESC";
+                }
             //echo $sql; //for debugging purposes
             
              $stmt = $conn->prepare($sql);
              $stmt->execute();
              $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
+        //pull before add
         
         //when displayed, post book name: bookDescription
             foreach ($records as $record) { 
@@ -89,6 +140,7 @@
             <label for="bookCat">Category: </label>
             <select id="bookCat" name="bookCategory">
               <option value="" >  Select One </option> 
+              <?php displayCategories(); ?> 
               
                <!--We need to get the list from the DB to be displayed.-->
                <!--  Something like getCategory() function may be needed.-->
@@ -99,10 +151,21 @@
             <label for="bookAuthor">Author: </label> 
             <select id="bookAuthor" name="bookAuthor">
               <option value="" > Select One </option> 
-             </select>
+                  <?php displayAuthor(); ?> 
+            </select>
+             
+             
+             
+             
+             
+             
              <p>Sort by: </p>
-             <input type="radio" name="orderBy" id = "author" value = "author"> <label>Author</label> <br>
-             <input type="radio" name="orderBy" id = "book" value = "book"> <label> Book</label> <br>
+             <input type="radio" name="sortBy" id = "author" value = "author"> <label>Author</label> <br>
+             <input type="radio" name="sortBy" id = "book" value = "book"> <label> Book</label> <br>
+             
+             <p>Order by: </p>
+             <input type="radio" name="orderBy" id = "asc" value = "asc"> <label> A-Z</label> <br>
+             <input type="radio" name="orderBy" id = "desc" value = "desc"> <label> Z-A</label> <br>
              <!--<input type="radio" name="orderBy" id = "cat" value = "cat"> <label> Category</label> <br>-->
 
               
