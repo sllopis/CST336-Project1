936602ba779d0c081cead4c013f7e05c5db3d5f4
Sean Quevedo <squevedo@csumb.edu>
2018-04-11 00:53:24 +0000

    added index and information.php

--- index.php
+++ index.php
@@ -1,7 +1,72 @@
+
 <?php
 
+    include 'dbConnection.php';
+    
+    $conn = getDatabaseConnection("tp");
 
+    function displaySearchResults(){
+        global $conn;
+        
+        if (isset($_GET['searchButton'])) { //checks whether user has submitted the form
+            
+            echo "<h3>Products Found: </h3>"; 
+            
+            //following sql works but it DOES NOT prevent SQL Injection
+            //$sql = "SELECT * FROM om_product WHERE 1
+            //       AND productName LIKE '%".$_GET['product']."%'";
+            
+            //Query below prevents SQL Injection
+            
+            //$namedParameters = array();
+            
+            $sql = "SELECT bookId, bookName, bookDescription FROM book "; //dont need WHERE 1?
+            
+            
+            if(isset($_GET['orderBy'])) {
+                
+                //SELECT * FROM book INNER JOIN category ON book.categoryID = category.catID ORDER BY category.catName
+                //join the authorID to author table
+                if($_GET['orderBy'] == "author") {
+                    $sql .= " INNER JOIN author ON book.authorID = author.authorID ORDER BY author.authorName";
+                }
+                
+                if($_GET['orderBy'] == "book") {
+                    $sql .= " WHERE 1 ORDER BY bookName";
+                }
+                
+                //join the categoryID to category table
+                
+                // SELECT * 
+                // FROM book 
+                // NATURAL JOIN category
+                // ON book.categoryID = catID
+                if($_GET['orderBy'] == "category") {
+                    $sql .= " ORDER BY category";
+                }
+                
+                
+            }
+            //echo $sql; //for debugging purposes
+            
+             $stmt = $conn->prepare($sql);
+             $stmt->execute();
+             $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        
+        //when displayed, post book name: bookDescription
+            foreach ($records as $record) { 
+                //add category //add 
+                // <a href = "\information.php?bookId=1> </a>"?
+                 echo "<a href =\"information.php?bookId=" . $record["bookId"] . "\"> Info </a>";
+                 echo  "<strong>" . $record["authorName"] . " " . $record["bookName"] . ":</strong>" . " " . $record["bookDescription"] . "<br /> <br>";
+            
+            }
+        }
+        
+    }
 
+    
 ?>
 
 
@@ -31,16 +96,23 @@
             <br>
             
             
-            <label for="bookAuthor">Author: </label>
+            <label for="bookAuthor">Author: </label> 
             <select id="bookAuthor" name="bookAuthor">
               <option value="" > Select One </option> 
+             </select>
+             <p>Sort by: </p>
+             <input type="radio" name="orderBy" id = "author" value = "author"> <label>Author</label> <br>
+             <input type="radio" name="orderBy" id = "book" value = "book"> <label> Book</label> <br>
+             <!--<input type="radio" name="orderBy" id = "cat" value = "cat"> <label> Category</label> <br>-->
+
               
               <!--We need to get the list from the DB to be displayed.-->
               <!--Something like getAuthor() function may be needed.-->
-            </select>
+            <!--</select>-->
             <br>
             
-            <input type="submit" name="searchButton" value="Search/> 
+            <input type="submit" name="searchButton" value="Search"/> 
+            <?= displaySearchResults(); ?>
             
             
         </form>
---
+++ information.php
@@ -0,0 +1,48 @@
+
+<?php
+    
+    include 'dbConnection.php';
+    $conn = getDatabaseConnection("tp");
+
+    $productId = $_GET['bookId'];
+
+    $sql = "SELECT * FROM `book`
+            INNER JOIN author  on book.authorID = author.authorID         
+            INNER JOIN category on book.categoryID = category.catID
+            WHERE bookId = :pId";    
+    
+    $np = array();
+    $np[":pId"] = $productId;
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($np);
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //title, description, author, category
+        
+        // echo $record['bookName'] . "<br>";
+        echo "<img src='" . $record['bookImage'] . "' width = '200' /><br/>";
+        echo "Title: " . $record["bookName"] . "<br />";
+        echo "Book Description: " . $record["bookDescription"] . "<br />";
+        echo "Author: " . $record["authorName"] . "<br />";       //how to get author name?
+        echo "Category: " . $record["catName"] . "<br />";
+        echo "Category Description: " . $record["catDescription"] . "<br />";
+    
+    
+    // if(sizeof($records) == 0) {
+    //     echo "No purchase information found, please direct back and choose another item!";
+    // }
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <link href ="css/styles.css" rel ="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+    <body>
+
+    </body>
+</html>
